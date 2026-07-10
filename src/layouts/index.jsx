7b68f7c7dd04d647d347e461ff43a490e6199a24
Kevin <288398693+ninjasilen@users.noreply.github.com>
2026-07-09 20:03:40 -0700

    feat: scroll to top

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,8 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function Layout() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }, [pathname]);
+
   return (
     <>
       <Header />
